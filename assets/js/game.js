82d91b45e0e2ffc160ec8d4bc08ee6619587460f
Jonathan Price <jonprice0@gmail.com>
2021-08-31 18:53:31 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,10 +71,11 @@ if (promptFight === "skip" || promptFight === "SKIP") {
 }
 };
 
-
-
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
 }
