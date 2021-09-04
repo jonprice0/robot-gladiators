8724bd33d795a022cb13b46598358b4111759afd
Jonathan Price <jonprice0@gmail.com>
2021-09-04 15:44:40 -0400

    resolved mixed-case input for the fight-or-skip prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,13 +5,17 @@
 //  * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
-var fight = function(enemy) {
-    while(playerInfo.health > 0 && enemy.health > 0) {
-
+var fightOrSkip = function() {
+    //ask player if they'd like to fight or skip using fightOrSkip function
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
+    if (!promptFight) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+promptFight = promptFight.toLowerCase();
 // if player choses to skip
-if (promptFight === "skip" || promptFight === "SKIP") {
+if (promptFight === "skip") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -20,10 +24,17 @@ if (promptFight === "skip" || promptFight === "SKIP") {
         window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
         // subtract money from playerInfo.money for skipping
         playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money);
-        break;
+        return true;
     }
 }
+return false;
+}
+
+var fight = function(enemy) {
+    while(playerInfo.health > 0 && enemy.health > 0) {
+        if (fightOrSkip()) {
+            break;
+        }
 
     // Subtract the value 'playerInfo.attack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
